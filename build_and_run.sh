1e2931ff39abf1fe2dd0dfb6f1517f26151e0956
celian <celian.riboulet@gmail.com>
2021-08-30 23:33:23 +0200

    Upgrade build & run script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,13 +1,47 @@
-cd backend
+#/bin/bash
 
-echo "----- [ STARTING BACKEND APPLICATION ] -----"
-echo "[ OK ] -- Build backend image"
-docker run -p 5000:5000 -v $(pwd)/db:/usr/src/app/db -d backend
-echo "[ OK ] -- Start backend container"
-docker run -p 5000:5000 -v $(pwd)/db:/usr/src/app/db -d backend
-echo "[ OK ] -- Backend started"
+red=`tput setaf 1`
+green=`tput setaf 2`
+cyan=`tput setaf 6`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
 
-cd ../hardware
+error () {
+    echo
+    echo
+    echo "${red}[[[ ERROR ]]]${reset}"
+    exit
+}
 
-echo "----- [ STARTING HARDWARE APPLICATION ] -----"
-echo "[ SKIP ] -- No suitable application yet"
+success () {
+    echo
+    echo "${green}$1${reset}"
+}
+
+title () {
+    echo
+    echo "${cyan}[ $1 ]${reset}"
+}
+
+step () {
+    echo
+    echo "${yellow}$1${reset}"
+    $2 || error
+}
+
+
+title "Starting backend application"
+
+if test "$(docker ps | wc -l)" -gt 1 ;then
+    step "Killing existing container" "docker rm $(docker stop $(docker ps -a -q --filter ancestor=backend --format="{{.ID}}"))"
+else
+    success "No container running found"
+fi
+
+step "Build backend image" "docker image build -t backend ./backend"
+
+step "Run backend container" "docker run -p 5000:5000 -v $(pwd)/backend/db:/usr/src/app/db -d backend"
+
+step "All running containers" "docker ps"
+
+success "APPLICATION STARTED"
