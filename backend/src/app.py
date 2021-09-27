846888f00a1111772b5c918e70e6eac11500a5cc
celian <celian.riboulet@gmail.com>
2021-09-27 19:30:08 +0200

    Backup check only on PROD env

--- backend/src/app.py
+++ backend/src/app.py
@@ -5,8 +5,6 @@ from api import api
 from database.database import init_db
 from backup import check_if_backup_required
 
-check_if_backup_required()
-
 if __name__ == '__main__':
     app = Flask(__name__)
 
@@ -14,6 +12,9 @@ if __name__ == '__main__':
     init_db()
 
     if os.environ.get('PROD') == None :
+        # Development schema
         app.run(debug=True)
     else :
+        # Production schema
+        check_if_backup_required()
         app.run(debug=False, port=5000, host="0.0.0.0")
