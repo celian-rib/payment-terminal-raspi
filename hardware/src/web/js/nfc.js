f0db0e4231279d0823c916bf06a9367e34142a7b
celian <celian.riboulet@gmail.com>
2021-09-29 20:15:16 +0200

    Refactor text animation on nfc page

--- hardware/src/web/js/nfc.js
+++ hardware/src/web/js/nfc.js
@@ -1,47 +1,44 @@
 let price = "loading ...";
 
 const base = "En attente de la carte";
-const delay = 200;
+const animation_tick = 200;
 
-window.onload = function() {
+const delay = async (duration) => new Promise((resolve) => setTimeout(() => resolve(), duration));
 
-    console.log(window.location.href)
-    const urlData = parseURLParams(window.location.href);
-    price = Object.keys(urlData)[0];
-    priceText.innerHTML = getPriceString(price);
-
-    // appel de la fonction de scan de carte
-    eel.await_card_scan(price);
-
-
+function textAnimation() {
     const text = document.getElementById("textWriter");
-    const anim = () => {
+    const anim = async () => {
         text.innerHTML = base
-        setTimeout(() => {
-            text.innerHTML = base + "."
-            setTimeout(() => {
-                text.innerHTML = base + ".."
-                setTimeout(() => {
-                    text.innerHTML = base + "..."
-                    setTimeout(() => {
-                        anim()
-                    }, delay)
-                }, delay)
-            }, delay)
-        }, delay)
+        await delay(animation_tick)
+        text.innerHTML = base + "."
+        await delay(animation_tick)
+        text.innerHTML = base + ".."
+        await delay(animation_tick)
+        text.innerHTML = base + "..."
+        await delay(animation_tick)
+        anim()
     }
     anim()
 }
 
-
 eel.expose(scan_complete)
-
 function scan_complete(money, userID) {
     goToWithParam("goToValidTransac", "?money=" + money + "&userID=" + userID);
 }
 
 eel.expose(scan_cancel)
-
 function scan_cancel(money, userID, reason) {
     goToWithParam("goToUnvalidTransac", "?money=" + money + "&userID=" + userID + "&reason=" + reason);
+}
+
+window.onload = function() {
+    // Retreive price from url
+    const urlData = parseURLParams(window.location.href);
+    price = Object.keys(urlData)[0];
+    priceText.innerHTML = getPriceString(price);
+
+    // Start transaction on python side
+    eel.await_card_scan(price);
+
+    textAnimation()
 }
