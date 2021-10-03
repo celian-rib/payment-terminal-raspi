87401075c68d5104a70932dce593cd428c9e8ebd
celian <celian.riboulet@gmail.com>
2021-10-03 22:52:09 +0200

    WIP

--- backend/src/backup.py
+++ backend/src/backup.py
@@ -1,63 +1,48 @@
 import requests
 import os
-import smtplib
 import socket
+import pysftp
 
-from datetime import datetime
 from pathlib import Path
-
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email.encoders import encode_base64
-
-BACKUP_MAIL_ADDRESS = "celian.riboulet@gmail.com"
+from cryptography.fernet import Fernet
 
 DB_PATH = str(Path(__file__).parent.parent) + "/db/prod.sqlite3"
 
 def internet_on():
     try:
-        requests.get('https://www.google.com/', timeout=1)
+        requests.get("https://google.com", timeout=1)
         return True
     except (requests.ConnectionError, requests.Timeout) as exception:
         return False
 
 def send_backup():
     print("Sending new backup...")
-    SUBJECT = "Asso-Card Backup " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    msg = MIMEMultipart()
-    msg['Subject'] = SUBJECT
-    msg['From'] = BACKUP_MAIL_ADDRESS
-    msg['To'] = BACKUP_MAIL_ADDRESS
+    key = list(os.environ.get('SFTP_PRIVATE_KEY'))
+    print(bytes(key))
+    fernet = Fernet(bytes(key))
+    host=os.environ.get('SFTP_HOST')
+    user=os.environ.get('SFTP_USERNAME')
+    pswd = fernet.decrypt(os.environ.get('SFTP_AUTH_CHAIN')).decode()
+    with pysftp.Connection(
+        host=host,
+        username=user, 
+        password=pswd
+    ) as sftp:
+        sftp.put(DB_PATH, os.environ.get('SFTP_HOST_PATH'))
 
-    part = MIMEBase('application', "octet-stream")
-    part.set_payload(open(DB_PATH, "rb").read())
-    encode_base64(part)
-    part.add_header(
-        'Content-Disposition',
-        'attachment; filename="backup.sqlite3"'
-    )
-
-    msg.attach(part)
-
-    session = smtplib.SMTP('smtp.gmail.com', 587)
-    session.starttls()
-    session.login(BACKUP_MAIL_ADDRESS, os.environ.get('SMTP_PASSWORD'))
-
-    session.sendmail(BACKUP_MAIL_ADDRESS, BACKUP_MAIL_ADDRESS, msg.as_string())
-
-    print("Backup sent to ", BACKUP_MAIL_ADDRESS)
+    print("Backup sent to ", host)
 
 def check_if_backup_required():
     if not internet_on():
         print("Backup check canceled : NO INTERNET")
         return
 
-    local_ip = str(socket.gethostbyname(socket.gethostname()))
-    if local_ip == "192.168.1.29" or local_ip == "192.168.1.37":
-        print("Backup canceled : Celian's home detected")
-        return
+    # local_ip = str(socket.gethostbyname(socket.gethostname()))
+    # if local_ip == "192.168.1.29" or local_ip == "192.168.1.37":
+    #     print("Backup canceled : Celian's home detected")
+    #     return
     
     try:
         send_backup()
     except Exception as e:
-        print("BACKUP ERROR", e)
+        print("[ BACKUP ERROR ]", e)
